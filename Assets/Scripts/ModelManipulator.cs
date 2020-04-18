df499899c54c7df6a22f4dc1e96f05f4a83150a4
Mikkomar <43349616+Mikkomar@users.noreply.github.com>
2020-04-18 13:19:25 +0300

    Model zooming

--- Assets/Scripts/ModelManipulator.cs
+++ Assets/Scripts/ModelManipulator.cs
@@ -19,15 +19,45 @@ public class ModelManipulator : MonoBehaviour
     void Update()
     {
 
-        /* If a countinuous finger press is detected */
-        if (Input.GetMouseButton(0))
+        /* If using only one finger */
+        if (Input.touchCount == 1)
         {
-            Vector3 direction = Input.mousePosition - TouchContinuesAt;
-            DebugLine($"Direction: {direction.ToString()}");
-            RotateModel(direction);
+            /* If a countinuous finger press is detected */
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 direction = Input.mousePosition - TouchContinuesAt;
+                DebugLine($"Direction: {direction.ToString()}");
+                RotateModel(direction);
+            }
+
+            TouchContinuesAt = Input.mousePosition;
         }
 
-        TouchContinuesAt = Input.mousePosition;
+        /* If using two fingers */
+        if(Input.touchCount == 2)
+        {
+            /* Get input for both fingers */
+            Touch TouchZero = Input.GetTouch(0);
+            Touch TouchOne = Input.GetTouch(1);
+
+            Vector2 TouchZeroPosition = TouchZero.position - TouchZero.deltaPosition;
+            Vector2 TouchOnePosition = TouchOne.position - TouchOne.deltaPosition;
+
+            float TouchMagnitude = (TouchZeroPosition - TouchOnePosition).magnitude;
+            float TouchDeltaMagnitude = (TouchZero.position - TouchOne.position).magnitude;
+
+            float MagnitudeDifference = TouchMagnitude - TouchDeltaMagnitude;
+
+            MoveModelRelatedToCamera(MagnitudeDifference);
+        }
+    }
+
+    private void MoveModelRelatedToCamera(float distance)
+    {
+        if(ActiveModel != null)
+        {
+            ActiveModel.position += Camera.main.transform.forward * distance;
+        }
     }
 
     private void RotateModel(Vector3 rotationDirection)
