bdaea82373aca39b75974cd2f4e7ba590b39b0b9
Mikkomar <43349616+Mikkomar@users.noreply.github.com>
2020-04-18 23:34:47 +0300

    Model switching
    Switching between models

--- Assets/Scripts/ModelController.cs
+++ Assets/Scripts/ModelController.cs
@@ -6,26 +6,43 @@ using UnityEngine.UI;
 public class ModelController : MonoBehaviour
 {
     public Dropdown Dropdown;
+    public Button PlaceButton;
     private ModelManipulator GestureTracker;
-    private Transform ActiveModel;
     // Start is called before the first frame update
     void Start()
     {
         HideChildren();
-        this.GestureTracker = GameObject.Find("GestureTracker").GetComponent<ModelManipulator>();
+        var parentClass = GameObject.Find("GestureTracker");
+        if (parentClass != null)
+        {
+            this.GestureTracker = parentClass.GetComponent<ModelManipulator>();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CheckPlaceButtonVisibility();
     }
 
+    /// <summary>
+    /// Hides clonable models on Start
+    /// </summary>
     private void HideChildren()
     {
         for(int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Changes PlaceButton visibility depending whether or not there is an active Model
+    /// </summary>
+    private void CheckPlaceButtonVisibility()
+    {
+        if (PlaceButton != null && GestureTracker != null)
+        {
+            PlaceButton.gameObject.SetActive(GestureTracker.ActiveModel != null);
         }
     }
 
@@ -36,23 +53,49 @@ public class ModelController : MonoBehaviour
     {
         if (this.Dropdown != null)
         {
+            /* Gets the selected dropdown list index, not counting the title option in index 0 */
             int DropDownValueIndex = Dropdown.value - 1;
+            /* If selected index not "Select Model" */
             if (DropDownValueIndex != -1)
             {
+                /* Selects the right clonable child Model based on the selected DDL index */
                 for(int i = 0; i < transform.childCount; i++)
                 {
                     GameObject Child = transform.GetChild(i).gameObject;
+                    /* If child has the same value as the selected DDL index, it's set as the active Model and cloned */
                     if(i == DropDownValueIndex)
                     {
-                        ActiveModel = Instantiate(Child).transform;
-                        ActiveModel.gameObject.SetActive(true);
                         if(GestureTracker != null)
                         {
-                            GestureTracker.ActiveModel = this.ActiveModel;
+                            /* If there is no active Model, the selected object is cloned, set as the active Model and made visible*/
+                            if (GestureTracker.ActiveModel == null)
+                            {
+                                GestureTracker.ActiveModel = Instantiate(Child).transform;
+                                GestureTracker.ActiveModel.gameObject.SetActive(true);
+                            }
+                            /* If there exists an active Model, it is replaced and deleted */
+                            else
+                            {
+                                var TempModel = Instantiate(Child).transform;
+                                TempModel.transform.position = GestureTracker.ActiveModel.transform.position;
+                                TempModel.transform.rotation = GestureTracker.ActiveModel.transform.rotation;
+                                TempModel.transform.localScale = GestureTracker.ActiveModel.transform.localScale;
+                                Destroy(GestureTracker.ActiveModel.gameObject);
+                                GestureTracker.ActiveModel = TempModel;
+                                GestureTracker.ActiveModel.gameObject.SetActive(true);
+                            }
                         }
                     }
                 }
             }
         }
+    }
+
+    /// <summary>
+    /// Places the active Model by setting it non-interactable
+    /// </summary>
+    public void PlaceObject()
+    {
+        this.GestureTracker.ActiveModel = null;
     }
 }
